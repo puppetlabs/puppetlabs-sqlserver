f176da78c68e3f7d7f76836a83d04e4b1f7b738a
Zach Reichert <zach.reichert@puppetlabs.com>
2014-10-16 15:01:39 -0700

    update iso location on smp share

--- spec/acceptance/manifests/SQL2012.pp
+++ spec/acceptance/manifests/SQL2012.pp
@@ -1,6 +1,6 @@
 # requires puppetlabs/powershell
 
 exec { 'samba_share':
-  command => 'Mount-DiskImage \\int-resources.ops.puppetlabs.net\Resources\ISO\Windows\SQL_Server\SQLServer2012SP1-FullSlipstream-ENU-x64.iso',
+  command => 'Mount-DiskImage \\int-resources.ops.puppetlabs.net\Resources\microsoft_sql\iso\SQLServer2012SP1-FullSlipstream-ENU-x64.iso',
   provider => powershell,
 }
