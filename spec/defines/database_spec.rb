e023e551b016e04c8ccb12e5e14089ce123b14cf
Travis Fields <travis@puppetlabs.com>
2014-11-10 10:56:00 -0800

    Refactor database checks and fix collation_name

--- spec/defines/database_spec.rb
+++ spec/defines/database_spec.rb
@@ -15,9 +15,15 @@ describe 'mssql::database', :type => :define do
 
   context 'Providing log filespec' do
     it 'should compile with valid log on params' do
-      params.merge!(log_filename: "c:/test/logfile.ldf")
-      params.merge!(log_name: "myCrazyLog")
-      should contain_mssql_tsql('database-MSSQLSERVER-myTestDb')
+      params.merge!({:log_filename => "c:/test/logfile.ldf"})
+      params.merge!({:log_name => "myCrazyLog"})
+      should contain_mssql_tsql('database-MSSQLSERVER-myTestDb').with_command(/c\:\/test\/logfile\.ldf/)
+    end
+  end
+  context 'adding collation check' do
+    it 'should compile and include collation name' do
+      params.merge!({:collation_name => 'SQL_Latin1_General_CP1_CI_'})
+      should contain_mssql_tsql('database-MSSQLSERVER-myTestDb').with_onlyif(/AND d\.collation_name = \'SQL_Latin1_General_CP1_CI_AS\'/)
     end
   end
 
