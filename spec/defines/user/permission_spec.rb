461ba1b1ac080305465f0306fce6edecf02be6f0
Travis Fields <travis@puppetlabs.com>
2015-02-18 10:10:27 -0800

    FM-2236 Add with_grant_option for user permission

--- spec/defines/user/permission_spec.rb
+++ spec/defines/user/permission_spec.rb
@@ -6,7 +6,7 @@ describe 'sqlserver::user::permission' do
   context 'validation errors' do
     include_context 'manifests' do
       let(:title) { 'myTitle' }
-      let(:sqlserver_tsql_title) { 'user-permissions-MSSQLSERVER-loggingDb-loggingUser-GRANT-SELECT' }
+      let(:sqlserver_tsql_title) { 'user-permissions-MSSQLSERVER-loggingDb-loggingUser-SELECT' }
     end
     context 'user =>' do
       let(:params) { {
@@ -24,6 +24,7 @@ describe 'sqlserver::user::permission' do
       end
       describe 'over limit' do
         let(:additional_params) { {:user => random_string_of_size(129)} }
+        it_behaves_like 'validation error'
       end
     end
     context 'permission' do
@@ -57,11 +58,29 @@ describe 'sqlserver::user::permission' do
         it_behaves_like 'validation error'
       end
     end
+    context 'with_grant_option => ' do
+      let(:params) { {
+          :permission => 'SELECT',
+          :database => 'loggingDb',
+          :user => 'loggingUser',
+
+      } }
+      describe 'true AND state => DENY' do
+        let(:additional_params) { {:with_grant_option => true, :state => 'DENY'} }
+        let(:raise_error_check) { "Can not use with_grant_option and state DENY, must be 'GRANT' " }
+        it_behaves_like 'validation error'
+      end
+      describe 'invalid' do
+        let(:additional_params) { {:with_grant_option => 'invalid'} }
+        let(:raise_error_check) { '"invalid" is not a boolean' }
+        it_behaves_like 'validation error'
+      end
+    end
   end
   context 'successfully' do
     include_context 'manifests' do
       let(:title) { 'myTitle' }
-      let(:sqlserver_tsql_title) { 'user-permissions-MSSQLSERVER-loggingDb-loggingUser-GRANT-SELECT' }
+      let(:sqlserver_tsql_title) { 'user-permissions-MSSQLSERVER-loggingDb-loggingUser-SELECT' }
       let(:params) { {
           :user => 'loggingUser',
           :permission => 'SELECT',
@@ -70,7 +89,7 @@ describe 'sqlserver::user::permission' do
     end
     %w(revoke grant deny).each do |state|
       context "state => '#{state}'" do
-        let(:sqlserver_tsql_title) { "user-permissions-MSSQLSERVER-loggingDb-loggingUser-#{state.upcase}-SELECT" }
+        let(:sqlserver_tsql_title) { 'user-permissions-MSSQLSERVER-loggingDb-loggingUser-SELECT' }
         let(:should_contain_command) { ["#{state.upcase} SELECT TO [loggingUser];", 'USE [loggingDb];'] }
         describe "lowercase #{state}" do
           let(:additional_params) { {:state => state} }
@@ -88,14 +107,14 @@ describe 'sqlserver::user::permission' do
       describe 'upper limit' do
         permission =random_string_of_size(128, false)
         let(:additional_params) { {:permission => permission} }
-        let(:sqlserver_tsql_title) { "user-permissions-MSSQLSERVER-loggingDb-loggingUser-GRANT-#{permission.upcase}" }
+        let(:sqlserver_tsql_title) { "user-permissions-MSSQLSERVER-loggingDb-loggingUser-#{permission.upcase}" }
         let(:should_contain_command) { ['USE [loggingDb];'] }
         it_behaves_like 'sqlserver_tsql command'
       end
       describe 'alter' do
         let(:additional_params) { {:permission => 'ALTER'} }
         let(:should_contain_command) { ['USE [loggingDb];', 'GRANT ALTER TO [loggingUser];'] }
-        let(:sqlserver_tsql_title) { "user-permissions-MSSQLSERVER-loggingDb-loggingUser-GRANT-ALTER" }
+        let(:sqlserver_tsql_title) { 'user-permissions-MSSQLSERVER-loggingDb-loggingUser-ALTER' }
         it_behaves_like 'sqlserver_tsql command'
       end
     end
@@ -110,12 +129,39 @@ describe 'sqlserver::user::permission' do
       it_behaves_like 'compile'
     end
 
+    context 'with_grant_option =>' do
+      describe 'true' do
+        let(:additional_params) { {:with_grant_option => true} }
+        let(:should_contain_command) { [
+            "IF @perm_state != 'GRANT_WITH_GRANT_OPTION'",
+            'GRANT SELECT TO [loggingUser] WITH GRANT OPTION;',
+        ] }
+        let(:should_not_contain_command) { [
+            'REVOKE GRANT OPTION FOR SELECT FROM [loggingUser];'] }
+        let(:should_contain_onlyif) { ["IF @perm_state != 'GRANT_WITH_GRANT_OPTION'",] }
+        it_behaves_like 'sqlserver_tsql command'
+        it_behaves_like 'sqlserver_tsql without_command'
+        it_behaves_like 'sqlserver_tsql onlyif'
+      end
+      describe 'false' do
+        let(:should_contain_command) { [
+            "IF @perm_state != 'GRANT'",
+            'GRANT SELECT TO [loggingUser];',
+            'REVOKE GRANT OPTION FOR SELECT TO [loggingUser] CASCADE;',
+            "IF 'GRANT_WITH_GRANT_OPTION' = ISNULL(",
+        ] }
+
+        let(:should_contain_onlyif) { ["IF @perm_state != 'GRANT'",] }
+        it_behaves_like 'sqlserver_tsql command'
+        it_behaves_like 'sqlserver_tsql onlyif'
+      end
+    end
   end
 
   context 'command syntax' do
     include_context 'manifests' do
       let(:title) { 'myTitle' }
-      let(:sqlserver_tsql_title) { 'user-permissions-MSSQLSERVER-loggingDb-loggingUser-GRANT-SELECT' }
+      let(:sqlserver_tsql_title) { 'user-permissions-MSSQLSERVER-loggingDb-loggingUser-SELECT' }
       let(:params) { {
           :user => 'loggingUser',
           :permission => 'SELECT',
@@ -128,7 +174,7 @@ describe 'sqlserver::user::permission' do
             /DECLARE @perm_state varchar\(250\)/,
             /SET @perm_state = ISNULL\(\n\s+\(SELECT perm.state_desc FROM sys\.database_principals princ\n\s+JOIN sys\./,
             /JOIN sys\.database_permissions perm ON perm\.grantee_principal_id = princ.principal_id\n\s+WHERE/,
-            /WHERE princ\.type in \('U','S','G'\) AND name = 'loggingUser' AND permission_name = 'SELECT' \),\n\s+'REVOKE'\);/,
+            /WHERE princ\.type in \('U','S','G'\) AND name = 'loggingUser' AND permission_name = 'SELECT'\),\n\s+'REVOKE'\)\s+;/,
             /DECLARE @error_msg varchar\(250\);\nSET @error_msg = 'EXPECTED user \[loggingUser\] to have permission \[SELECT\] with GRANT but got ' \+ @perm_state;/,
             /IF @perm_state != 'GRANT'\n\s+THROW 51000, @error_msg, 10/
         ] }
