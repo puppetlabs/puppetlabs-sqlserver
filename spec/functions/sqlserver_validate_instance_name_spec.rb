25ac79cc107ebea49d330ebde512b556fde78470
Travis Fields <travis@puppetlabs.com>
2015-02-25 11:21:58 -0800

    Fixed a bug with sqlserver_validate_instance_name that was not catching empty strings

--- spec/functions/sqlserver_validate_instance_name_spec.rb
+++ spec/functions/sqlserver_validate_instance_name_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 describe 'sqlserver_validate_instance_name function' do
   let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+
   it 'should exist' do
     expect(Puppet::Parser::Functions.function("sqlserver_validate_instance_name")).to eq("function_sqlserver_validate_instance_name")
   end
+
   it 'should fail with over 16 characters' do
     expect { scope.function_sqlserver_validate_instance_name('ABCDEFGHIJKLMNOPQRSTUVWXYZ') }.to raise_error
+  end
+
+  it 'should fail empty string' do
+    expect { scope.function_sqlserver_validate_instance_name('') }.to raise_error
   end
 
 end
