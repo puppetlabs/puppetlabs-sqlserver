71309842cd6c02ff672bbba10b2ddba685e02151
Travis Fields <travis@puppetlabs.com>
2015-03-04 09:47:41 -0800

    FM-2299 Login - Allow for permissions hash

--- spec/defines/login/permissions_spec.rb
+++ spec/defines/login/permissions_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'manifest_shared_examples.rb'))
 
-describe 'sqlserver::login::permission' do
+describe 'sqlserver::login::permissions' do
   let(:facts) { {:osfamily => 'windows'} }
   context 'validation errors' do
     include_context 'manifests' do
@@ -10,11 +10,11 @@ describe 'sqlserver::login::permission' do
     end
     context 'login =>' do
       let(:params) { {
-          :permissions=> ['SELECT'],
+        :permissions => ['SELECT'],
       } }
       let(:raise_error_check) { 'Login must be between 1 and 128 characters' }
       describe 'missing' do
-        let(:raise_error_check) { 'Must pass login to Sqlserver::Login::Permission[myTitle]' }
+        let(:raise_error_check) { 'Must pass login to Sqlserver::Login::Permissions[myTitle]' }
         it_behaves_like 'validation error'
       end
       describe 'empty' do
@@ -28,26 +28,26 @@ describe 'sqlserver::login::permission' do
     end
     context 'permission' do
       let(:params) { {
-          :login => 'loggingUser',
+        :login => 'loggingUser',
       } }
       let(:raise_error_check) { 'Permission must be between 4 and 128 characters' }
       describe 'empty' do
-        let(:additional_params) { {:permissions=> ['']} }
+        let(:additional_params) { {:permissions => ['']} }
         it_behaves_like 'validation error'
       end
       describe 'under limit' do
-        let(:additional_params) { {:permissions=> [random_string_of_size(3, false)]} }
+        let(:additional_params) { {:permissions => [random_string_of_size(3, false)]} }
         it_behaves_like 'validation error'
       end
       describe 'over limit' do
-        let(:additional_params) { {:permissions=> [random_string_of_size(129, false)]} }
+        let(:additional_params) { {:permissions => [random_string_of_size(129, false)]} }
         it_behaves_like 'validation error'
       end
     end
     context 'state =>' do
       let(:params) { {
-          :permissions=> ['SELECT'],
-          :login => 'loggingUser'
+        :permissions => ['SELECT'],
+        :login => 'loggingUser'
       } }
       describe 'invalid' do
         let(:additional_params) { {:state => 'invalid'} }
@@ -61,8 +61,8 @@ describe 'sqlserver::login::permission' do
       let(:title) { 'myTitle' }
       let(:sqlserver_tsql_title) { 'login-permission-MSSQLSERVER-loggingUser-GRANT' }
       let(:params) { {
-          :login => 'loggingUser',
-          :permissions=> ['SELECT'],
+        :login => 'loggingUser',
+        :permissions => ['SELECT'],
       } }
     end
     %w(revoke grant deny).each do |state|
@@ -90,7 +90,7 @@ describe 'sqlserver::login::permission' do
         it_behaves_like 'sqlserver_tsql command'
       end
       describe 'alter' do
-        let(:additional_params) { {:permissions=> ['ALTER']} }
+        let(:additional_params) { {:permissions => ['ALTER']} }
         let(:should_contain_command) { ['USE [master];', 'GRANT ALTER TO [loggingUser];'] }
         let(:sqlserver_tsql_title) { "login-permission-MSSQLSERVER-loggingUser-GRANT" }
         it_behaves_like 'sqlserver_tsql command'
@@ -113,19 +113,19 @@ describe 'sqlserver::login::permission' do
       let(:title) { 'myTitle' }
       let(:sqlserver_tsql_title) { 'login-permission-MSSQLSERVER-loggingUser-GRANT' }
       let(:params) { {
-          :login => 'loggingUser',
-          :permissions => ['SELECT'],
+        :login => 'loggingUser',
+        :permissions => ['SELECT'],
       } }
       describe '' do
         let(:should_contain_command) { [
-            'USE [master];',
-            'GRANT SELECT TO [loggingUser];',
-            /DECLARE @perm_state varchar\(250\)/,
-            /SET @perm_state = ISNULL\(\n\s+\(SELECT perm.state_desc FROM sys\.server_permissions perm\n\s+JOIN sys\./,
-            /JOIN sys\.server_principals princ ON princ.principal_id = perm\.grantee_principal_id\n\s+WHERE/,
-            /WHERE princ\.type IN \('U','S','G'\)\n\s+ AND princ\.name = 'loggingUser'\n\s+AND perm\.permission_name = @permission\),\n\s+'REVOKE'\)/,
-            /SET @error_msg = 'EXPECTED login \[loggingUser\] to have permission \[' \+ @permission \+ '\] with GRANT but got ' \+ @perm_state;/,
-            /IF @perm_state != 'GRANT'\n\s+THROW 51000, @error_msg, 10/
+          'USE [master];',
+          'GRANT SELECT TO [loggingUser];',
+          /DECLARE @perm_state varchar\(250\)/,
+          /SET @perm_state = ISNULL\(\n\s+\(SELECT perm.state_desc FROM sys\.server_permissions perm\n\s+JOIN sys\./,
+          /JOIN sys\.server_principals princ ON princ.principal_id = perm\.grantee_principal_id\n\s+WHERE/,
+          /WHERE princ\.type IN \('U','S','G'\)\n\s+ AND princ\.name = 'loggingUser'\n\s+AND perm\.permission_name = @permission\),\n\s+'REVOKE'\)/,
+          /SET @error_msg = 'EXPECTED login \[loggingUser\] to have permission \[' \+ @permission \+ '\] with GRANT but got ' \+ @perm_state;/,
+          /IF @perm_state != 'GRANT'\n\s+THROW 51000, @error_msg, 10/
         ] }
         it_behaves_like 'sqlserver_tsql command'
       end
