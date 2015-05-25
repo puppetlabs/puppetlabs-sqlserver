374a00d9df4c5e5cad82f2a888eb452a7dbaddae
Zach Reichert <zach.reichert@puppetlabs.com>
2015-05-25 14:52:24 -0700

    (maint) modify base_install to work with string and integer for version

--- spec/sql_testing_helpers.rb
+++ spec/sql_testing_helpers.rb
@@ -77,13 +77,13 @@ end
 
 def base_install(sql_version)
   case sql_version
-  when /2012/
+  when /2012/, 2012
     iso_opts = {
       :qa_iso_resource_root   => QA_RESOURCE_ROOT,
       :sqlserver_iso          => SQL_2012_ISO,
       :sqlserver_version      => '2012',
     }
-  when /2014/
+  when /2014/, 2012
     iso_opts = {
       :qa_iso_resource_root   => QA_RESOURCE_ROOT,
       :sqlserver_iso          => SQL_2014_ISO,
