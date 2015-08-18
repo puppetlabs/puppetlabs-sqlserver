ce0eb22d9e26353c9f1834c17520960d07ab2200
phongdly <phong@puppetlabs.com>
2015-08-18 13:38:27 -0700

    (MODULES-2391) SQLSERVER - Create Automated Tests For sqlserver_instance 2015-08-26

---
+++ spec/acceptance/sqlserver_instance_spec.rb
@@ -0,0 +1,134 @@
+require 'spec_helper_acceptance'
+require 'securerandom'
+require 'erb'
+
+host = find_only_one("sql_host")
+inst_name = "MSSQL" + SecureRandom.hex(4)
+inst_name = inst_name.upcase
+
+describe "sqlserver_instance", :node => host do
+  version = host['sql_version'].to_s
+
+  def ensure_sqlserver_instance(host, features, inst_name, ensure_val = 'present')
+    manifest = <<-MANIFEST
+    sqlserver_instance{'<%= inst_value %>':
+      name                  => '<%= inst_value %>',
+      ensure                => <%= ensure_value %>,
+      source                => 'H:',
+      features              => [ <%= mssql_features %> ],
+      sql_sysadmin_accounts => ['Administrator'],
+      agt_svc_account       => 'Administrator',
+      agt_svc_password      => 'Qu@lity!',
+    }
+    MANIFEST
+
+    inst_value      = inst_name
+    ensure_value    = ensure_val
+    mssql_features  = features.map{ |x| "'#{x}'"}.join(', ')
+
+    pp = ERB.new(manifest).result(binding)
+
+    apply_manifest_on(host, pp) do |r|
+      expect(r.stderr).not_to match(/Error/i)
+    end
+  end
+
+  context "can create sqlserver instance: #{inst_name}" do
+
+    features = ['SQL', 'SQLEngine', 'Replication', 'FullText', 'DQ']
+
+    before(:all) do
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+      remove_sql_features(host, {:features => features, :version => version})
+    end
+
+    after(:all) do
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+      remove_sql_features(host, {:features => features, :version => version})
+    end
+
+    it "create #{inst_name} instance" do
+      ensure_sqlserver_instance(host, features, inst_name)
+
+      validate_sql_install(host, {:version => version}) do |r|
+        expect(r.stdout).to match(/#{Regexp.new(inst_name)}/)
+      end
+    end
+
+    it "remove #{inst_name} instance" do
+      remove_sql_features(host, {:features => features, :version => version})
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+
+      validate_sql_install(host, {:version => version}) do |r|
+        expect(r.stdout).not_to match(/#{Regexp.new(inst_name)}/)
+      end
+    end
+  end
+
+  context "can create instance with only SQL feature" do
+    features = ['SQL']
+
+    before(:all) do
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+      remove_sql_features(host, {:features => features, :version => version})
+    end
+
+    after(:all) do
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+      remove_sql_features(host, {:features => features, :version => version})
+    end
+
+    it "create #{inst_name} instance" do
+      ensure_sqlserver_instance(host, features, inst_name)
+
+      validate_sql_install(host, {:version => version}) do |r|
+        expect(r.stdout).to match(/#{Regexp.new(inst_name)}/)
+      end
+    end
+  end
+
+  context "can create instance with only RS feature" do
+    features = ['RS']
+
+    before(:all) do
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+      remove_sql_features(host, {:features => features, :version => version})
+    end
+
+    after(:all) do
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+      remove_sql_features(host, {:features => features, :version => version})
+    end
+
+    it "create #{inst_name} instance" do
+      ensure_sqlserver_instance(host, features, inst_name)
+
+      validate_sql_install(host, {:version => version}) do |r|
+        expect(r.stdout).to match(/#{Regexp.new(inst_name)}/)
+      end
+    end
+  end
+
+  context "can create instance with only AS feature" do
+    features = ['AS']
+
+    before(:all) do
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+      remove_sql_features(host, {:features => features, :version => version})
+    end
+
+    after(:all) do
+      ensure_sqlserver_instance(host, features, inst_name, 'absent')
+      remove_sql_features(host, {:features => features, :version => version})
+    end
+
+    it "create #{inst_name} instance" do
+      ensure_sqlserver_instance(host, features, inst_name)
+
+      validate_sql_install(host, {:version => version}) do |r|
+        expect(r.stdout).to match(/#{Regexp.new(inst_name)}/)
+      end
+    end
+  end
+
+end
