dafb807338e6ee403e0ee6527d5b127d75c8e021
Hunter Haugen <hunter@puppetlabs.com>
2015-12-15 20:18:22 -0800

    Add error message output

--- lib/puppet/type/sqlserver_tsql.rb
+++ lib/puppet/type/sqlserver_tsql.rb
@@ -113,7 +113,7 @@ Puppet::Type::newtype(:sqlserver_tsql) do
         @output = provider.run_update
       end
       unless @output.exitstatus.to_s == "0"
-        self.fail("#{self.resource[:command]} returned #{@output.exitstatus} instead of one of [#{self.should.join(",")}]")
+        self.fail("#{self.resource[:command]} returned #{@output.exitstatus} instead of one of [#{self.should.join(",")}] and error output #{@output.error_message}")
       end
       event
     end
