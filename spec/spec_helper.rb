a40009f42628406207cfb67827f862a683f4d666
sheena <sheena@puppet.com>
2019-09-17 14:17:12 +0100

    MODULES-9599(adding changelog generator)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,8 +55,4 @@ def ensure_module_defined(module_name)
   end
 end
 
-def regexp_matches(available_parameters)
-    match(available_parameters)
-end
-
 # 'spec_overrides' from sync.yml will appear below this line
