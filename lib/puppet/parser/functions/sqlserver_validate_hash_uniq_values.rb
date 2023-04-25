2d5a6c3c2165d2b95f64eb2fdde8c58599addfbe
jordanbreen28 <jordan.breen@perforce.com>
2023-04-25 11:27:50 +0100

    (CONT-800) - Correct Style/TernaryParentheses

--- lib/puppet/parser/functions/sqlserver_validate_hash_uniq_values.rb
+++ lib/puppet/parser/functions/sqlserver_validate_hash_uniq_values.rb
@@ -15,7 +15,7 @@ module Puppet::Parser::Functions
 
     total_count = value.count
     uniq_count = value.uniq.count
-    msg = (arguments[1]) ? arguments[1] : "Duplicate values passed to hash #{value}"
+    msg = arguments[1] || "Duplicate values passed to hash #{value}"
     raise(Puppet::ParseError, msg) if uniq_count != total_count
   end
 end
